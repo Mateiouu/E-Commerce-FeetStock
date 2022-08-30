c1b0ac77c27ceb96dbb8e74f011d61b22d20788d
Mateiouu <mateo.doherty@hotmail.com.ar>
2022-08-29 21:13:09 -0300

    cambiando un error de nombre de url

--- FeetStock/accounts/urls.py
+++ FeetStock/accounts/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
     path('signup/', views.registerView, name = 'register'),
     path('password/', CambiarContraView.as_view(template_name = 'change_password.html')),
-    path('editar_perfil/', UserEditView.as_view(), name = 'editar-perfil'),
+    path('profile/', UserEditView.as_view(), name = 'editar-perfil'),
     path('ordenes/', userpage, name = "ordenes"),
 ]
 
